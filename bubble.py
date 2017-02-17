6e5ff1189219a66099ffdcecd2951104d94745a6
Jose <jootse.84@gmail.com>
2017-02-17 17:55:23 -0500

    Radix Sort

--- bubble.py
+++ bubble.py
@@ -1,13 +1,16 @@
-def bubble(l_elems, is_sorted, step):
+def bubble(elems, is_sorted, step):
     if step is 1 or is_sorted:
-        return l_elems
+        # base case: elems is already sorted or we pass through the list len(elems) times
+        return elems
     else:
         is_swapped = False
-        for i in range(len(l_elems)):
-            if i < len(l_elems) - 1 and l_elems[i] > l_elems[i + 1]:
+        for i in range(len(elems) - 1):
+            # compares each pair of adjacent items and swaps them if they are in the wrong order
+            if elems[i] > elems[i + 1]:
                 is_swapped = True
-                l_elems[i], l_elems[i + 1] = l_elems[i + 1], l_elems[i]
-        return bubble(l_elems, not is_swapped, step - 1)
+                elems[i], elems[i + 1] = elems[i + 1], elems[i]
+        # if is_swapped is True, the algorithm needs to pass through the list again
+        return bubble(elems, not is_swapped, step - 1)
 
 print(bubble(range(10,0,-1), False, len(range(10,0,-1))))
 print(bubble([5,6,5,3,3], False, len([5,6,5,3,3])))
---
+++ radix.js
@@ -0,0 +1,63 @@
+const countingSort = (input) => {
+  /***
+   * from https://en.wikipedia.org/wiki/Counting_sort
+   * variables:
+   *    input -- the array of items to be sorted; key(x) returns the key for item x
+   *    n -- the length of the input
+   *    k -- a number such that all keys are in the range 0..k-1
+   *    count -- an array of numbers, with indexes 0..k-1, initially all zero
+   *    output -- an array of items, with indexes 0..n-1
+   *    x -- an individual input item, used within the algorithm
+   *    total, oldCount, i -- numbers used within the algorithm
+   ***/
+
+  var n = input.length
+  var k = Math.max.apply(null, input) + 1
+  var count = Array(k).fill().map((_, i) => 0)
+  var output = Array(n)
+  var total = 0
+
+  // calculate the histogram of key frequencies
+  for (var value of input) {
+    count[value] += 1
+  }
+
+  // calculate the starting index for each key
+  // so count[input[val]] will return the index of the position it
+  // should store the value in the result - output 
+  for (var i of Array(k).fill().map((_, i) => i)) {
+    // i = 0, 1, ... k-1
+    var oldCount = count[i]
+    count[i] = total
+    total += oldCount
+  }
+
+  // copy to output array, preserving order of inputs with equal keys
+  // NOTE: for this case we save keys instead of value!!!
+  for (var key of input.keys()) {
+    output[count[input[key]]] = key
+    count[input[key]] += 1
+  }
+
+  return output
+}
+
+const radixSort = (input) => {
+  var max = Math.max.apply(null, input.map((x) => x.toString().length))
+
+  for (var i of Array(max).fill().map((_, i) => i)) {
+    var output = Array(input.length)
+    var partition = input.map((x) => x.toString().length > i ? x % Math.pow(10, i + 1) : 0)
+
+    countingSort(partition).forEach((elem, index) => {
+      output[index] = input[elem]
+    })
+    input = output
+    console.log(`wave${i} `, input)
+  }
+  return input
+}
+
+input = [4, 1016, 401, 189, 4]
+console.log('input: ', input)
+console.log('output: ', radixSort(input))
